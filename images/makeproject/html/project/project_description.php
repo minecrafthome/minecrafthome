7dcdbcebbe431e32f9107d2907a704ff694e54bc
Chip Wolf <hello@chipwolf.uk>
2020-06-17 16:47:46 +0100

    fix(site): add project description

---
+++ images/makeproject/html/project/project_description.php
@@ -0,0 +1,3 @@
+<?php
+print("This is a volunteer computing project which allows anyone on the internet to contribute resources to research projects related to Minecraft.<br /><br />Minecraft@Home enables the study of the fundemental laws of Minecraft to answer unanswered questions regarding the features and true limits of the game.<br />");
+?>
