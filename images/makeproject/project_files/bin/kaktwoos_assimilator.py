93e0b586dc83c56320e68f0c58e74d936975b275
DutChen18 <dutchen18@gmail.com>
2020-07-04 14:45:11 +0200

    fix(assimilator): change output directory

--- images/makeproject/project_files/bin/kaktwoos_assimilator.py
+++ images/makeproject/project_files/bin/kaktwoos_assimilator.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 from assimilator import *
-import re
+from Boinc import boinc_project_path
+import re, os
 
 re_unit = re.compile(r"--chunkseed (\d+)")
 re_result = re.compile(r"^(\d+)\r?$", re.MULTILINE)
@@ -11,7 +12,12 @@ class KaktwoosAssimilator(Assimilator):
 
 	def assimilate_handler(self, wu, results, canonical_result):
 		chunkseed = re_unit.search(wu.xml_doc).group(1)
-		with open("results.txt", "a") as f:
+		path = boinc_project_path.project_path("kaktwoos_results")
+		try:
+			os.makedirs(path)
+		except OSError:
+			pass
+		with open(os.path.join(path, "results.txt"), "a") as f:
 			for match in re_result.finditer(canonical_result.stderr_out):
 				f.write("{} {}\n".format(chunkseed, match.group(1)))
 
