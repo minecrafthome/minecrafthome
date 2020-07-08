c80ff187d20a08be8c0df1dc3d6dc9ae0cfb9921
Chip Wolf <hello@chipwolf.uk>
2020-07-08 20:29:43 +0100

    feat: add badge assignment

---
+++ images/makeproject/project/html/ops/badge_assign_mch.php
@@ -0,0 +1,21 @@
+<?php
+require_once("../inc/util_ops.inc");
+
+$badges = BoincBadge::enum("");
+
+foreach ($badges as $badge) {
+    $type = $badge->type?"team":"user";
+
+    if ($type == "user") {
+        $items = BoincUser::enum(null, "where $badge->sql_rule");
+    } else {
+        $items = BoincTeam::enum(null, "where $badge->sql_rule");
+    }
+
+    foreach ($items as $item) {
+        echo "Assigning $badge->name to $type $item->id \n";
+        assign_badge($badge->type?False:True, $item, $badge);
+    }
+}
+
+?>
