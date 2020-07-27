bffc4806518943cc91e1ea268bf8cefbe6cdcb62
BoySanic <boysonicrevived@gmail.com>
2020-07-27 00:33:52 -0400

    feat(boinc): new validator and assimilator kaktwoos validator based on mrspike's kaktwoos_validator  - Made some edits to fix his work kaktoos validator based on mrspike's kaktwoos_validator  - Adapted this to work for kaktoos, and implemented fixes from kaktwoos_validator kaktoos assimilator based on dutchen's kaktwoos_assimilator  - Adapted this to work for kaktoos with his advice for regex
    dummy_script.py is our replacement for kaktwoos_validator_syntax and kaktoos_validator_syntax as we don't feel we need to check syntax.
    It simply returns 0 to say the syntax is good.
    
    Hy provided original adaptation of assimilator
    
    Co-authored-by: MrSpike63 <mrspike1782@gmail.com>
    Co-authored-by: DutChen18 <dutchen18@gmail.com>
    Co-authored-by: Hyenadae <hypprs@gmail.com>

---
+++ images/makeproject/project/bin/kaktwoos_validator_compare.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import sys
+import re
+import boinc_path_config
+from Boinc import database
+
+# argv[1] and argv[2] contain the file we want to check
+result_1 = sys.argv[1]
+result_2 = sys.argv[2]
+print result_1
+print result_2
+database.connect_default_config()
+file_1 = database.Results.find(id=result_1)[0]
+file_2 = database.Results.find(id=result_2)[0]
+
+
+print("{}".format(file_1))
+print("{}".format(file_2))
+# Strip the files of unneeded information (checkpoints, workunit) by extracting seed
+seeds_1 = re.findall("Found seed: ([\s\S]*?)\n", file_1.stderr_out)
+seeds_2 = re.findall("Found seed: ([\s\S]*?)\n", file_2.stderr_out)
+
+# Remove duplicates (checkpoints can cause these) and sort the lists
+seeds_1 = sorted(list(set(seeds_1)))
+seeds_2 = sorted(list(set(seeds_2)))
+
+# Exit correctly
+if seeds_1 == seeds_2:
+    sys.exit(0)
+else:
+    sys.exit(1)
