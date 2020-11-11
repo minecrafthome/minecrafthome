6f01c64b62f862d155f57c22f797206d7a9192d8
BoySanic <boysonicrevived@gmail.com>
2020-11-11 07:56:19 -0500

    fix(assimilator): correct error logging
    Added helper functions from sample assimilator for logging.
    Added a critical log call when the OS cannot access files
    Moved file write and file open for the output file into the try/catch section

--- images/makeproject/project/bin/kaktoos_assimilator.py
+++ images/makeproject/project/bin/kaktoos_assimilator.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 from assimilator import *
-from Boinc import boinc_project_path, boinc_db
+from Boinc import boinc_project_path, boinc_db, sched_messages
 import re, os
 
 
@@ -9,7 +9,29 @@ re_name = re.compile(r"_y(\d+)_", re.MULTILINE)
 class KaktoosAssimilator(Assimilator):
         def __init__(self):
                 Assimilator.__init__(self)
-
+        def _writeLog(self, mode, *args):
+            """
+            A private helper function for writeing to the log
+            """
+            self.log.printf(mode, *args)
+        
+        def logCritical(self, *messageArgs):
+            """
+            A helper function for logging critical messages
+            """
+            self._writeLog(sched_messages.CRITICAL, *messageArgs)
+    
+        def logNormal(self, *messageArgs):
+            """
+            A helper function for logging normal messages
+            """
+            self._writeLog(sched_messages.NORMAL, *messageArgs)
+    
+        def logDebug(self, *messageArgs):
+            """
+            A helper function for logging debug messages
+            """
+            self._writeLog(sched_messages.DEBUG, *messageArgs)
         def assimilate_handler(self, wu, results, canonical_result):
                 if canonical_result == None:
                         return
@@ -23,12 +45,12 @@ class KaktoosAssimilator(Assimilator):
                     with open(input_path) as input_file:
                         input_str = input_file.read()
                     os.makedirs(path)
+                    with open(os.path.join(path, filename), "a") as f:
+                        for match in re_result.finditer(input_str):
+                            f.write("{}\n".format(match.group(1)))
                 except OSError:
-                        pass
-                with open(os.path.join(path, filename), "a") as f:
-		        for match in re_result.finditer(input_str):
-                                f.write("{}\n".format(match.group(1)))
-
+                    logCritical("Unable to open file: %s\n", OSError.filename)
+                    pass
 if __name__ == "__main__":
         asm = KaktoosAssimilator()
         asm.run()
