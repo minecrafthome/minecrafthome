f2b07552643212d726b4adabd8219a73cfd21efc
BoySanic <boysonicrevived@gmail.com>
2020-11-14 16:57:23 -0500

    fix(kaktoos-assimilator): move file open into try (#60)
    * fix(kaktoos-assimilator): move file open into try
    
    Moved file open function call into the try/catch block below it for fault tolerance in the case that the assimilator is unable to open the file.
    
    * fix(assimilator): correct error logging
    
    Added helper functions from sample assimilator for logging.
    Added a critical log call when the OS cannot access files
    Moved file write and file open for the output file into the try/catch section
    
    * fix(kaktoos_assimilator): fixed faulty try/except
    
    Split each IO action into 3 parts to catch all 3 error points separately in more detail
    Changed except OSError to except OSError as e
    Fixed references to OSError.filename to e.filename
    
    Co-authored-by: Neil <h66R10@gmail.com>

--- images/makeproject/project/bin/kaktoos_assimilator.py
+++ images/makeproject/project/bin/kaktoos_assimilator.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 from assimilator import *
-from Boinc import boinc_project_path, boinc_db
+from Boinc import boinc_project_path, boinc_db, sched_messages
 import re, os
 
 
@@ -9,7 +9,29 @@ re_name = re.compile(r"_y(\d+)_", re.MULTILINE)
 class KaktoosAssimilator(Assimilator):
         def __init__(self):
                 Assimilator.__init__(self)
-
+        def _writeLog(self, mode, *args):
+            """
+            A private helper function for writeing to the log
+            """
+            self.log.printf(mode, *args)
+        
+        def logCritical(self, *messageArgs):
+            """
+            A helper function for logging critical messages
+            """
+            self._writeLog(sched_messages.CRITICAL, *messageArgs)
+    
+        def logNormal(self, *messageArgs):
+            """
+            A helper function for logging normal messages
+            """
+            self._writeLog(sched_messages.NORMAL, *messageArgs)
+    
+        def logDebug(self, *messageArgs):
+            """
+            A helper function for logging debug messages
+            """
+            self._writeLog(sched_messages.DEBUG, *messageArgs)
         def assimilate_handler(self, wu, results, canonical_result):
                 if canonical_result == None:
                         return
@@ -18,18 +40,24 @@ class KaktoosAssimilator(Assimilator):
                 heightmatch = re_name.search(canonical_result.name)
                 height = heightmatch.group(1)
                 filename = "results_" + height + ".txt"
-
-                with open(input_path) as input_file:
-                    input_str = input_file.read()
+                input_str = ""
+                try:
+                    with open(input_path) as input_file:
+                        input_str = input_file.read()
+                except OSError as e:
+                    logCritical("Unable to open input file: %s\n", e.filename)
 
                 try:
-                        os.makedirs(path)
-                except OSError:
-                        pass
-                with open(os.path.join(path, filename), "a") as f:
-		        for match in re_result.finditer(input_str):
-                                f.write("{}\n".format(match.group(1)))
+                    os.makedirs(path)
+                except OSError as e:
+                    logCritical("Unable to create output directory: %s\n", e.filename)
 
+                try:
+                    with open(os.path.join(path, filename), "a") as f:
+                        for match in re_result.finditer(input_str):
+                            f.write("{}\n".format(match.group(1)))
+                except OSError as e:
+                    logCritical("Unable to write to output file: %s\n", e.filename)
 if __name__ == "__main__":
         asm = KaktoosAssimilator()
         asm.run()
