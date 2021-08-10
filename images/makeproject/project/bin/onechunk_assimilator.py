03dfe7610634613bfdace0ca81f515aba273c0bd
xxMattewxx <xxmattewxx@gmail.com>
2021-08-10 17:16:13 -0300

    Fix OneChunk assimilator not saving results correctly.

--- images/makeproject/project/bin/onechunk_assimilator.py
+++ images/makeproject/project/bin/onechunk_assimilator.py
@@ -3,30 +3,33 @@ from assimilator import *
 from Boinc import boinc_project_path
 import re, os
 
-re_result = re.compile(r"^(\d+)\r?$", re.MULTILINE)
-
 class OneChunkAssimilator(Assimilator):
         def __init__(self):
                 Assimilator.__init__(self)
 
         def assimilate_handler(self, wu, results, canonical_result):
-                if canonical_result == None:
-                        return
-
-                path = boinc_project_path.project_path("onechunk_results")
-                input_path = self.get_file_path(canonical_result)
-
-                with open(input_path) as input_file:
-                    input_str = input_file.read()
-
                 try:
-                    os.makedirs(path)
-                except OSError:
-                    pass
+                    if canonical_result == None:
+                            return
 
-                with open(os.path.join(path, "results.txt"), "a") as f:
-                    for match in re_result.finditer(input_str):
-                        f.write("{}\n".format(match.group(1)))
+                    path = boinc_project_path.project_path("onechunk_results")
+                    input_path = self.get_file_path(canonical_result)
+
+                    with open(input_path) as input_file:
+                        input_str = input_file.read()
+
+                    try:
+                        os.makedirs(path)
+                    except OSError:
+                        pass
+
+                    lines = set()
+                    with open(os.path.join(path, "results.txt"), "a") as f:
+                        for line in input_str.splitlines():
+                            lines.add(line)
+                        for line in lines:
+                            f.write("{}\n".format(line))
+                except Exception as e: print(e)
 
 if __name__ == "__main__":
         asm = OneChunkAssimilator()
